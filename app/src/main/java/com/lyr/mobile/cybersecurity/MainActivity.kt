11f0d57f020d6077391f6b7066d9a0fef37cd295
LiangYuanRui <lyr021x@gmail.com>
2023-05-24 20:46:30 +0800

    check if the Xposed was loaded

--- app/src/main/java/com/lyr/mobile/cybersecurity/MainActivity.kt
+++ app/src/main/java/com/lyr/mobile/cybersecurity/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.lyr.mobile.cybersecurity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.reflect.Field
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // check if the Xpose was loaded
+    fun checkXposed() : Boolean {
+        val xpose = ClassLoader.getSystemClassLoader()
+                    .loadClass("de.robv.android.xposed.XposedBridge")
+        return xpose != null
     }
 
 
